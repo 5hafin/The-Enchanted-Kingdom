a10a1449bfb1994b4569ea8bd0c4483eda58bd2e
5hafin <Sky-Shift@yandex.ru>
2023-11-06 01:12:20 +0700

    кликабельное стартовое меню

---
+++ player/ui/Interface.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_saves_pressed():
+	pass # Replace with function body.
+
+
+func _on_guid_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://player/ui/options_interface.tscn")
